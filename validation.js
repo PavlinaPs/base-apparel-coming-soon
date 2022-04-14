a47772e0379aaf0862af74e844cf4852f6567343
PPS <pavlina.psarsky@gmail.com>
2022-04-14 18:05:08 +0200

    initial commit, mobile layout and email validation done

---
+++ validation.js
@@ -0,0 +1,23 @@
+const validationRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //w3resource
+
+let enteredEmail = document.getElementById("email");
+const form = document.getElementById('form');
+const errorMessage = document.querySelector('.error-message');
+const errorSubmit = document.getElementById('submit');
+
+
+form.addEventListener('submit', (e) => {
+    if (validationRegex.test(enteredEmail.value)) {
+        enteredEmail.classList.remove('email-error');
+        errorMessage.setAttribute('visibility', 'hidden');
+        errorSubmit.setAttribute('data-error', false);
+        enteredEmail.value = null;
+        return;
+    } else {
+        e.preventDefault();
+        enteredEmail.classList.add('email-error');
+        errorMessage.setAttribute('visibility', 'visible');
+        errorSubmit.setAttribute('data-error', true);
+        return;
+    }
+});
